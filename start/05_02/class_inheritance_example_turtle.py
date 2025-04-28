5a20ccde44336e16e43d61e19499a1eead59bc6e
Dattu Hoover <hooverd21@students.ecu.edu>
2025-04-28 17:11:51 +0000

    modified:   start/05_02/class_inheritance_example_turtle.py 	modified:   start/05_03/class_inheritance_challenge.py 	modified:   start/06_02/polymorphism_example.py 	modified:   start/06_03/poly_challenge.py

--- start/05_02/class_inheritance_example_turtle.py
+++ start/05_02/class_inheritance_example_turtle.py
@@ -3,9 +3,11 @@ import random
 
 
 class RainbowTurtle(turtle.Turtle):
-    def __init__(self):
+    def __init__(self, x, y):
         super().__init__()
         self.shape("turtle")
+        self.penup()
+        self.goto(x, y)
         self.color = random.choice(colors)
         self.onclick(self.change_color)
         self.fillcolor(random.choice(colors))
@@ -21,5 +23,13 @@ class RainbowTurtle(turtle.Turtle):
 
 colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
 
-my_turtle = RainbowTurtle()
+my_turtle1 = RainbowTurtle(-200, 200)
+my_turtle2 = RainbowTurtle(200, -200)
+my_turtle3 = RainbowTurtle(200, 200)
+my_turtle4 = RainbowTurtle(-200, -200)
+
 turtle.done()
+# The code creates a turtle graphics program where turtles change color when clicked.
+# The turtles are positioned at different coordinates on the screen.
+# The colors are randomly chosen from a predefined list of rainbow colors.
+# The turtles are created using a class that inherits from the turtle module's Turtle class.
--- start/05_03/class_inheritance_challenge.py
+++ start/05_03/class_inheritance_challenge.py
@@ -21,7 +21,22 @@ class Animal:
 
 # Subclass Dog
 
+
+class Dog(Animal):
+    def __init__(self, name, sound):
+        super().__init__(name, sound)
+
+    def wag_tail(self):
+        print(f"{self.name} is wagging its tail!")
 # Subclass Cat
+
+
+class Cat(Animal):
+    def __init__(self, name, sound):
+        super().__init__(name, sound)
+
+    def purr(self):
+        print(f"{self.name} is purring!")
 
 
 # Create instances
